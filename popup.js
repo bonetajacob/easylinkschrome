a5c4b75e2885eb457533e8c1d52482fb2ee5dcf2
Jacob Boneta <boneta.jacob@gmail.com>
2018-10-29 03:02:19 -0600

    added .js function draw + openweathermap API (JSON) i hope this doesnt mess anything up

--- popup.js
+++ popup.js
@@ -1,1 +1,19 @@
 // Future JavaScript will go here
+var weather;
+function setup() {
+  createCanvas(400,400);
+  loadJSON('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8e4e30cdc0202200584cdd65fd24c743&&units=imperial',gotData);
+}
+
+function gotData(data)  {
+  //println(data);
+  weather = data;
+}
+
+function draw() {
+    background(0);
+    if (weather) {
+      ellipse(100,100,weath.main.temp, weather.main.temp)
+      ellipse(300,100,weath.main.humidity, weather.main.humidity)
+    }
+}
