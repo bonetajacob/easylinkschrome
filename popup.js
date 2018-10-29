16f3167bcd8e193e5fb6b007766c0c04de26fbae
Jacob Boneta <boneta.jacob@gmail.com>
2018-10-29 03:32:28 -0600

    i believe i have hidden my api key by moving key into config.js and .gitignore + referenced the variable in "popup.js" then  include the config script in the html... hope this worked

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 var weather;
 function setup() {
   createCanvas(400,400);
-  loadJSON('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8e4e30cdc0202200584cdd65fd24c743&&units=imperial',gotData);
+  loadJSON('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid='+ config.apiKey + '&units=imperial',gotData);
 }
 
 function gotData(data)  {
